0efce3685b9f020e32c60946124ca978ff22567b
Igor Brehm <igorbrehm@hotmail.com>
2019-04-06 18:33:14 -0300

    implementado funcao score

--- Labyrinth_Problem.java
+++ Labyrinth_Problem.java
@@ -17,7 +17,8 @@ public class Labyrinth_Problem {
     public Labyrinth_Problem() throws FileNotFoundException{
         File file = new File(System.getProperty("user.dir")+"/lab_exemplo1.txt");
         Scanner in = new Scanner(file);
-        best_path = new String[]{"N","N","N","N","N","N","N","N","N","N"};
+        this.best_path = new String[10];
+        this.best_path = initialize_path(best_path);
         this.dimension = in.nextInt();
         this.labyrinth = new String[dimension][dimension];
         for(int i = 0; i < dimension; i++){
@@ -29,6 +30,27 @@ public class Labyrinth_Problem {
         simulated_Annealing();
     }
     
+    private String[] initialize_path(String[] path){
+        Random random = new Random();
+        int j = 0;
+        for(int i = 0; i < path.length; i++){
+            j = random.nextInt(4);
+            if(j == 1){ // 1
+                path[i] = "L";
+            }
+            else if(j == 2){ // 2
+                path[i] = "R";
+            }
+            else if(j == 3){ // 3
+                path[i] = "U";
+            }
+            else{ // 4
+                path[i] = "D";
+            }
+        }
+        return path;
+    }
+    
     // Este metodo calcula o quao boa e a sequencia de direcoes sendo avaliada
     /*
      * Score: 
@@ -36,29 +58,112 @@ public class Labyrinth_Problem {
      * 2. Penalizar movimentos em 'circulo': Cada comportamento circular detectado penaliza em 10% do total de posicoes do labirinto
      * 3. Chega na saida? Ganha muito score: automaticamente recebe 80% do total de posicoes no score (sem levar em conta bonus e penalidades).
      */
-    public int evaluated_Distribution(int[] path){
+    private int evaluated_Distribution(String[] path){
         int score = 0;
-        int percent = Math.round((1/dimension)*100); 
-        for(int i = 0; i < dimension; i++){
-            for(int j = 0; j < dimension; j++){
-                /*
-                if(){ // 1
-                    score = score + (percent * 5);        
-                }
-                else if(){ // 2
-                    
-                }
-                else if(){ // 3
-                    
-                }
-                else{
-                    continue;    
-                }
-                */
+        int percent = Math.round((1/dimension)*100);
+        int current_x = 0;
+        int current_y = 0;
+        for(int k = 0; k < path.length; k++){
+            int[] feedback = new int[3];
+            feedback = move(path[k], current_x, current_y);
+            current_x = feedback[1];
+            current_y = feedback[2];
+            if(feedback[0] == 0){
+                break; //bateu
+            }
+            else if(feedback[0] == 1){ // 1
+                score = score + (percent * 5);
+            }
+            else if (feedback[0] == 2){ // 2
+                score = score - (percent * 10);
+            }
+            else{ // 3
+                score = score + (percent * 80);
+                break; //chegou na saida
             }
         }
         System.out.println("Score: "+ score);
         return score;
+    }
+    
+    private int[] move(String movement, int position_x, int position_y){
+        int[] exit_status = new int[3];
+        int x;
+        int y;
+        if(movement.equals("L")){ // Left
+            // checar se sai fora do labirinto com o movimento
+            // se nao sair, checar se a posicao tem o que escrito
+            if(labyrinth[position_x][position_y].equals("0") || labyrinth[position_x][position_y].equals("E")){ // 1
+                exit_status = {1,x,y};
+                return exit_status;    
+            }
+            else if(){ // 2
+                exit_status = {2,x,y};
+                return exit_status;
+            }
+            else if(){ // 3
+                exit_status = {3,x,y};
+                return exit_status;
+            }
+            else{
+                exit_status = {0,x,y};
+                return exit_status;
+            }
+        }
+        else if(movement.equals("R")){ // Right
+            if(labyrinth[position_x][position_y].equals("0") || labyrinth[position_x][position_y].equals("E")){ // 1
+                exit_status = {1,x,y};
+                return exit_status;    
+            }
+            else if(){ // 2
+                exit_status = {2,x,y};
+                return exit_status;
+            }
+            else if(){ // 3
+                exit_status = {3,x,y};
+                return exit_status;
+            }
+            else{
+                exit_status = {0,x,y};
+                return exit_status;
+            }
+        }
+        else if(movement.equals("U")){ // Up
+            if(labyrinth[position_x][position_y].equals("0") || labyrinth[position_x][position_y].equals("E")){ // 1
+                exit_status = {1,x,y};
+                return exit_status;    
+            }
+            else if(){ // 2
+                exit_status = {2,x,y};
+                return exit_status;
+            }
+            else if(){ // 3
+                exit_status = {3,x,y};
+                return exit_status;
+            }
+            else{
+                exit_status = {0,x,y};
+                return exit_status;
+            }
+        }
+        else(D){ // Down
+            if(labyrinth[position_x][position_y].equals("0") || labyrinth[position_x][position_y].equals("E")){ // 1
+                exit_status = {1,x,y};
+                return exit_status;    
+            }
+            else if(){ // 2
+                exit_status = {2,x,y};
+                return exit_status;
+            }
+            else if(){ // 3
+                exit_status = {3,x,y};
+                return exit_status;
+            }
+            else{
+                exit_status = {0,x,y};
+                return exit_status;
+            }
+        }
     }
     
     // Este metodo tenta encontrar um estado vizinho (combinacao de direcoes com apenas 2 direcoes de diferenca do estado atual)
@@ -109,17 +214,14 @@ public class Labyrinth_Problem {
     
     // Metodo que simula o algoritmo de tempera simulada
     public void simulated_Annealing(){
-        return;
-        /*
         double T = 300;
         for(int t = 1; t < 10000; t++){  
             find_Successor(T);
-            if(evaluated_Distribution(work_hours) == 0){
+            if(evaluated_Distribution(work_hours) >= (dimension * dimension)){
                 break;
             }    
             T = T*0.99;
         }
-        */
     }
     
     //Metodo para escrever na tela o labirinto atual
